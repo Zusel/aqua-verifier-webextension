09af87aae128b6bf5c11f1e8fb0a60c84733d9d3
rht <rhtbot@protonmail.com>
2021-08-11 05:04:00 -0400

    Detect more events to trigger badge update

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,7 @@
 import { setBadge } from "./set_badge";
 
+// https://stackoverflow.com/questions/60545285/how-to-use-onupdated-and-onactivated-simultanously
+
 function setBadgeWrapper(tabId: number) {
   chrome.tabs.get(tabId, function(tab) {
     const urlObj = new URL(tab.url || '');
@@ -7,6 +9,14 @@ function setBadgeWrapper(tabId: number) {
   });
 }
 
-chrome.tabs.onActivated.addListener((activeInfo) => {
-  setBadgeWrapper(activeInfo.tabId);
+chrome.tabs.onActivated.addListener((info) => {
+  setBadgeWrapper(info.tabId);
+});
+
+chrome.tabs.onUpdated.addListener((info) => {
+  setBadgeWrapper(info.tabId);
+});
+
+chrome.tabs.onCreated.addListener((info) => {
+  setBadgeWrapper(info.tabId);
 });

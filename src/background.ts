5de65fa485f680f91ab600adfcda400bbfbc4da2
rht <rhtbot@protonmail.com>
2021-08-11 05:19:59 -0400

    background: Use proper arguments for each tab trigger events

--- src/background.ts
+++ src/background.ts
@@ -2,21 +2,21 @@ import { setBadge } from "./set_badge";
 
 // https://stackoverflow.com/questions/60545285/how-to-use-onupdated-and-onactivated-simultanously
 
-function setBadgeWrapper(tabId: number) {
-  chrome.tabs.get(tabId, function(tab) {
+function setBadgeWrapper(tab: any) {
     const urlObj = new URL(tab.url || '');
     setBadge(urlObj);
-  });
 }
 
 chrome.tabs.onActivated.addListener((info) => {
-  setBadgeWrapper(info.tabId);
+  chrome.tabs.get(info.tabId, function(tab) {
+    setBadgeWrapper(tab);
+  });
 });
 
-chrome.tabs.onUpdated.addListener((info) => {
-  setBadgeWrapper(info.tabId);
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  setBadgeWrapper(tab);
 });
 
-chrome.tabs.onCreated.addListener((info) => {
-  setBadgeWrapper(info.tabId);
+chrome.tabs.onCreated.addListener((tab) => {
+  setBadgeWrapper(tab);
 });
